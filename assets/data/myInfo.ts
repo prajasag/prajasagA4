2c8833c206aab80bfd25e6b85bfa22d70b071c26
prajasag <52717608+prajasag@users.noreply.github.com>
2019-08-12 19:36:46 -0400

    Add files via upload

---
+++ assets/data/myInfo.ts
@@ -0,0 +1,9 @@
+import { prajasag } from 'src/app/prajasag';
+
+export const myInfo: prajasag={
+    studentNumber:'991490822',
+     name:'Sagar Prajapati', 
+     loginName:'prajasag', 
+     campus:'Davis', 
+     assgnTitle:'Assignment-4'
+}
